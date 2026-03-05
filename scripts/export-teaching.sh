6e6b526e25bbe0b9f47b2465f6d11dc9102b9a30
Brent Pappas <pappasbrent@gmail.com>
2026-03-05 18:02:42 -0500

    Change teaching formatting

--- scripts/export-teaching.sh
+++ scripts/export-teaching.sh
@@ -13,14 +13,15 @@ title: Teaching
 
 EOF
     echo "<ul>"
-    while IFS=$'\t' read -r name uni season year sitelink ytlink; do
+    while IFS=$'\t' read -r name uni season year sitelink ytlink spilink; do
         cat <<EOF
         <li>
             <p>
                 <span style="font-weight: bold">$name.</span>
                 $uni $season $year.
-                <a href="$sitelink">Course homepage.</a>
-                <a href="$ytlink">Lecture recordings.</a>
+                $(if [[ "$sitelink" != "None" ]]; then echo "<a href=\"$sitelink\">Course homepage.</a>"; fi)
+                $(if [[ "$ytlink" != "None" ]]; then echo "<a href=\"$ytlink\">Lecture recordings.</a>"; fi)
+                $(if [[ "$spilink" != "None" ]]; then echo "<a href=\"$spilink\">Student perception of instruction.</a>"; fi)
             </p>
         </li>
 EOF
@@ -28,8 +29,16 @@ EOF
     echo "</ul>"
 elif [[ "$1" == "typst" ]]; then
     echo "#let teaching = ["
-    while IFS=$'\t' read -r name uni season year sitelink ytlink; do
-        echo "#box[*$name*. $uni $season $year. #link(\"$sitelink\")[Course homepage.] #link(\"$ytlink\")[Course recordings.]]"
-    done < <(tail -n+2 tsv/teaching.tsv)
+    while IFS=$'\t' read -r name uni season year sitelink ytlink spilink; do
+        cat <<EOF
+        #box[
+            *$name*.
+            $uni $season $year.
+            $(if [[ "$sitelink" != "None" ]]; then echo "#link(\"$sitelink\")[Course homepage.]"; fi)
+            $(if [[ "$ytlink" != "None" ]]; then echo "#link(\"$ytlink\")[Lecture Recordings.]"; fi)
+            $(if [[ "$spilink" != "None" ]]; then echo "#link(\"$spilink\")[Student perception of instruction]"; fi)
+        ]
+EOF
+    done < <(tail -n+2 tsv/teaching.tsv) | sed '/^[[:space:]]*$/d'
     echo "]"
 fi
